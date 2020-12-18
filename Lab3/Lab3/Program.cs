8b3fbfa5ccea2c24306ca49c044c41cbf7fc2648
NetherQuartz <vd-larkin@ya.ru>
2020-12-19 01:10:16 +0300

    Исправлено освещение

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -296,19 +296,16 @@ public abstract class Application : CGApplication
             switch (Shading)
             {
                 case Shadings.ConstColor:
-                    color = Color.FromArgb(255, (int) red, (int) green, (int) blue);
+                    color = Color.FromArgb((int) red, (int) green, (int) blue);
                     e.Surface.DrawTriangle(color.ToArgb(), a, b, c);
                     break;
                 case Shadings.Flat:
-                    var i = Ia * Ka;
+                    var i = DVector3.Multiply(Ia, Ka);
                     R = DVector3.Reflect(-L.ToDVector3(), -polygon.Normal.ToDVector3());
                     cos = watcher.DotProduct(R) / (watcher.GetLength() * R.GetLength());
                     prod = DVector3.DotProduct(L.ToDVector3(), -polygon.Normal.ToDVector3());
-                    if (prod > 0)
-                    {
-                        i += Il * (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K);
-                    }
-                    color = Color.FromArgb(255, ColorMul(red, i.X), ColorMul(green, i.Y), ColorMul(blue, i.Z));
+                    i += DVector3.Multiply(Il, (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K));
+                    color = Color.FromArgb(ColorMul(red, i.X), ColorMul(green, i.Y), ColorMul(blue, i.Z));
                     e.Surface.DrawTriangle(color.ToArgb(), a, b, c);
                     break;
                 case Shadings.Gouraud:
@@ -316,21 +313,24 @@ public abstract class Application : CGApplication
                     R = DVector3.Reflect(-L.ToDVector3(), -VerticesNormals[polygon.P1].ToDVector3());
                     cos = watcher.DotProduct(R) / (watcher.GetLength() * R.GetLength());
                     prod = DVector3.DotProduct(L.ToDVector3(), -VerticesNormals[polygon.P1].ToDVector3());
-                    var i1 = Ia * Ka + Il * (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K);
+                    var i1 = DVector3.Multiply(Ia, Ka);
+                    i1 += DVector3.Multiply(Il, (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K));
                     
                     R = DVector3.Reflect(-L.ToDVector3(), -VerticesNormals[polygon.P2].ToDVector3());
                     cos = watcher.DotProduct(R) / (watcher.GetLength() * R.GetLength());
                     prod = DVector3.DotProduct(L.ToDVector3(), -VerticesNormals[polygon.P2].ToDVector3());
-                    var i2 = Ia * Ka + Il * (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K);
+                    var i2 = DVector3.Multiply(Ia, Ka);
+                    i2 += DVector3.Multiply(Il, (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K));
                     
                     R = DVector3.Reflect(-L.ToDVector3(), -VerticesNormals[polygon.P3].ToDVector3());
                     cos = watcher.DotProduct(R) / (watcher.GetLength() * R.GetLength());
                     prod = DVector3.DotProduct(L.ToDVector3(), -VerticesNormals[polygon.P3].ToDVector3());
-                    var i3 = Ia * Ka + Il * (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K);
+                    var i3 = DVector3.Multiply(Ia, Ka);
+                    i3 += DVector3.Multiply(Il, (Kd * prod + Ks * Math.Pow(cos, P)) / (d + K));
                     
-                    var c1 = Color.FromArgb(255, ColorMul(red, i1.X), ColorMul(green, i1.Y), ColorMul(blue, i1.Z));
-                    var c2 = Color.FromArgb(255, ColorMul(red, i2.X), ColorMul(green, i2.Y), ColorMul(blue, i2.Z));
-                    var c3 = Color.FromArgb(255, ColorMul(red, i3.X), ColorMul(green, i3.Y), ColorMul(blue, i3.Z));
+                    var c1 = Color.FromArgb(ColorMul(red, i1.X), ColorMul(green, i1.Y), ColorMul(blue, i1.Z));
+                    var c2 = Color.FromArgb(ColorMul(red, i2.X), ColorMul(green, i2.Y), ColorMul(blue, i2.Z));
+                    var c3 = Color.FromArgb(ColorMul(red, i3.X), ColorMul(green, i3.Y), ColorMul(blue, i3.Z));
                     
                     e.Surface.DrawTriangle(c1.ToArgb(), a.X, a.Y, c2.ToArgb(), b.X, b.Y, c3.ToArgb(), c.X, c.Y);
                     
